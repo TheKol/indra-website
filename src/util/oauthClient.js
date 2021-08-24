7e2bd6c48a49a4f479818f202e0367105c50976a
R Indra Akbar S <rindraakbars@gmail.com>
2021-08-24 10:30:59 +0700

    Fix error oauth Google

--- src/util/oauthClient.js
+++ src/util/oauthClient.js
@@ -3,5 +3,5 @@ import { google } from 'googleapis';
 export const oauthClient = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  `http://${process.env.PATH_CONFIG}/auth/google/callback`
+  `http://ec2-54-169-163-161.ap-southeast-1.compute.amazonaws.com/auth/google/callback`
 );
